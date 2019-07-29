12e96a4f52bc1ebf1faa58d6acc10f4c22a63e06
Jan Langheimer <langheimer@gmail.com>
2019-07-29 14:26:20 +0200

    remove tests

--- __tests__/bancorx.test.ts
+++ __tests__/bancorx.test.ts
@@ -1,39 +1,5 @@
 import * as bancorx from "..";
 
-test("bancorx.bancorFormula - EOS/BNT", () => {
-    const balanceFrom = 77814.0638; // EOS
-    const balanceTo = 429519.5539120331; // BNT
-    const amount = 1;
-    expect(bancorx.bancorFormula(balanceFrom, balanceTo, amount)).toBe(5.519748143058556);
-});
-
-test("bancorx.bancorInverseFormula - EOS/BNT", () => {
-    const balanceFrom = 77814.0638; // EOS
-    const balanceTo = 429519.5539120331; // BNT
-    const amountDesired = 1;
-    expect(bancorx.bancorInverseFormula(balanceFrom, balanceTo, amountDesired)).toBe(0.18116577989712823);
-});
-
-test("bancorx.parseMemo", () => {
-    const {CUSD, BNT} = bancorx.relays;
-    const minReturn = "3.17";
-    const destAccount = "<account>";
-    const version = 1;
-
-    // Single converter (BNT => CUSD)
-    expect(bancorx.parseMemo([CUSD], minReturn, destAccount))
-        .toBe("1,bancorc11144 CUSD,3.17,<account>");
-
-    expect(bancorx.parseMemo([CUSD], minReturn, destAccount, version))
-        .toBe("1,bancorc11144 CUSD,3.17,<account>");
-
-    // Multi converter (EOS => BNT => CUSD)
-    expect(bancorx.parseMemo([BNT, CUSD], minReturn, destAccount, version))
-        .toBe("1,bnt2eoscnvrt BNT bancorc11144 CUSD,3.17,<account>");
-
-});
-
-test("bancorx.parseBalance", () => {
-    expect(bancorx.parseBalance("10.0000 EOS")).toEqual({quantity: 10.0, symbol: "EOS"});
-    expect(bancorx.parseBalance(10.0)).toEqual({quantity: 10.0});
+test("bancorx.relays - CUSD", () => {
+    expect(bancorx.relays.CUSD).toBe({ code: "stablecarbon", account: "bancorc11144", symbol: "CUSD", precision: 2 });
 });
