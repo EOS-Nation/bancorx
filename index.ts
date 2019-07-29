c322f2c1282d7f2132d52d06c95d25ea4f3fad04
Jan Langheimer <langheimer@gmail.com>
2019-07-29 14:26:39 +0200

    reworked bancorx helper functions

--- index.ts
+++ index.ts
@@ -1,15 +1,14 @@
-import { Api, JsonRpc, RpcError } from "eosjs";
-const eosRpc = new JsonRpc("nodes.get-scatter.com", {});
+import { vxm } from "@/store";
 
 /**
  * Bancor X
  *
  * @param {string} from token to convert FROM
  * @param {string} to token to convert TO
- * @param {number} amountFrom amount to convert FROM
- * @param {string} destAccount destination acccount
+ * @param {number} amount amount to convert FROM
+ * @param {string} receiver destination acccount
  * @param {number} [version=1] bancor protocol version
- * @returns {object} min return amount and parsed memo
+ * @returns {object} min return amount and parsed bancor memo
  * @example
  *
  * // Convert EOS => CUSD
@@ -19,14 +18,52 @@ const eosRpc = new JsonRpc("nodes.get-scatter.com", {});
 export async function bancorX(
   from: string,
   to: string,
-  amountFrom: number,
-  destAccount: string,
-  version= 1,
+  amount: number,
+  receiver?: string,
+  version = 1,
 ) {
     //
     // GET RELAY BALANCES
     //
+    const {
+        balanceFrom,
+        balanceBnt,
+        balanceBntFrom,
+        balanceTo,
+    } = await relayBalances(from, to);
 
+    //
+    // CALCULATE MIN RETURN AMOUNT
+    //
+    // get BTN Amount
+    const amountBntFrom = bancorFormula(balanceFrom, balanceBnt, amount);
+
+    // get TO Amount
+    const minReturn = tokenPrecision(
+      to,
+      bancorFormula(balanceBntFrom, balanceTo, amountBntFrom),
+    );
+
+    //
+    // PARSE BANCOR MEMO
+    //
+    let parsedMemo = "";
+    if (receiver) { parsedMemo = bancorMemo(from, to, minReturn, receiver, version); }
+
+    return { minReturn, parsedMemo };
+}
+
+/**
+ * Relay Balances - get current balances of needed relays
+ *
+ * @param {string} from token symbol to convert FROM
+ * @param {string} to token symbol to convert TO
+ * @returns {{ number, number, number, number }} object with relay balances
+ * @example
+ *
+ * // Calculate min return BNT
+ */
+export async function relayBalances(from: string, to: string) {
     // Get Relays
     const relayFrom = relays[from];
     const relayTo = relays[to];
@@ -34,7 +71,7 @@ export async function bancorX(
 
     // Get Relay Balance FROM
     const balanceFrom = parseFloat(
-      await eosRpc.get_currency_balance(
+      await vxm.eosTransit.accessContext.eosRpc.get_currency_balance(
         relayFrom.code,
         relayFrom.account,
         relayFrom.symbol,
@@ -43,7 +80,7 @@ export async function bancorX(
 
     // Get Relay Balance BNT FROM
     const balanceBnt = parseFloat(
-      await eosRpc.get_currency_balance(
+      await vxm.eosTransit.accessContext.eosRpc.get_currency_balance(
         relayBnt.code,
         relayFrom.account,
         relayBnt.symbol,
@@ -52,7 +89,7 @@ export async function bancorX(
 
     // Get Relay Balance BNT TO
     const balanceBntFrom = parseFloat(
-      await eosRpc.get_currency_balance(
+      await vxm.eosTransit.accessContext.eosRpc.get_currency_balance(
         relayBnt.code,
         relayTo.account,
         relayBnt.symbol,
@@ -61,24 +98,97 @@ export async function bancorX(
 
     // Get Relay Balance TO
     const balanceTo = parseFloat(
-      await eosRpc.get_currency_balance(
+      await vxm.eosTransit.accessContext.eosRpc.get_currency_balance(
         relayTo.code,
         relayTo.account,
         relayTo.symbol,
       ),
     );
 
+    return { balanceFrom, balanceBnt, balanceBntFrom, balanceTo };
+}
+
+/**
+ * Bancor Memo - parse bancor memo
+ *
+ * @param {string} from symbol to convert FROM
+ * @param {string} to symbol to convert TO
+ * @param {string} receiver account to credit after conversion
+ * @param {number} minReturn min return amount
+ * @param {number} [version=1] bancor protocol version
+ * @returns {string} parsed bancor memo
+ * @example
+ *
+ * // Calculate min return BNT
+ */
+export function bancorMemo(
+  from: string,
+  to: string,
+  minReturn: number,
+  receiver: string,
+  version = 1,
+) {
+    // Get Relays
+    const relayFrom = relays[from];
+    const relayTo = relays[to];
+    //
+    // PARSE MEMO
+    //
+    if (from === "BNT") {
+        return `${version},${relayTo.account} ${
+          relayTo.symbol
+          },${minReturn},${receiver}`;
+    }
+    else if (to === "BNT") {
+        return `${version},${relayFrom.account} ${
+          relayTo.symbol
+          },${minReturn},${receiver}`;
+ }
+    else {
+        return `${version},${relayFrom.account} BNT ${relayTo.account} ${
+          relayTo.symbol
+          },${minReturn},${receiver}`;
+ }
+}
+
+/**
+ * Bancor Formula - calculate min return
+ *
+ * @param {number} balanceFrom relay FROM balance
+ * @param {number} balanceTo relay TO balance
+ * @param {number} amount amount to convert FROM
+ * @returns {number} conversion min return amount
+ * @example
+ *
+ * // Calculate min return BNT
+ */
+export function bancorFormula(
+  balanceFrom: number,
+  balanceTo: number,
+  amount: number,
+) {
     //
     // CALCULATE MIN RETURN AMOUNT
     //
+    return (amount / (balanceFrom + amount)) * balanceTo;
+}
 
-    // get BTN Amount
-    const amountBntFrom =
-      (amountFrom / (balanceFrom + amountFrom)) * balanceBnt;
+/**
+ * Set Token Precision - set correct bancor token decimal precision for given asset
+ *
+ * @param {string} symbol symbol of asset
+ * @param {number} amount amount to set precision
+ * @returns {number} amount with correct decimal precision
+ * @example
+ *
+ * // Calculate min return BNT
+ */
+export function tokenPrecision(symbol: string, amount: number) {
+    const amountSplit = amount.toString().split(".");
 
-    // get TO Amount
-    const amountTo =
-      (amountBntFrom / (balanceBntFrom + amountBntFrom)) * balanceTo;
+    amountSplit[1] = amountSplit[1].substring(0, relays[symbol].precision);
+
+    return parseFloat(amountSplit.join("."));
 }
 
 /**
@@ -227,28 +337,28 @@ export const relays: Relays = {
         precision: 8,
     },
     NUT: {
-       code: "eosdtnutoken",
-       account: "bancorc11215",
-       symbol: "NUT",
-       precision: 9,
+        code: "eosdtnutoken",
+        account: "bancorc11215",
+        symbol: "NUT",
+        precision: 9,
     },
     EOSDT: {
-       code: "eosdtsttoken",
-       account: "bancorc11222",
-       symbol: "EOSDT",
-       precision: 9,
+        code: "eosdtsttoken",
+        account: "bancorc11222",
+        symbol: "EOSDT",
+        precision: 9,
     },
     STUFF: {
-       code: "stuff.eos",
-       account: "bnr123553535",
-       symbol: "STUFF",
-       precision: 4,
+        code: "stuff.eos",
+        account: "bnr123553535",
+        symbol: "STUFF",
+        precision: 4,
     },
     FINX: {
-       code: "finxtokenvci",
-       account: "bnr215453322",
-       symbol: "FINX",
-       precision: 8,
+        code: "finxtokenvci",
+        account: "bnr215453322",
+        symbol: "FINX",
+        precision: 8,
     },
     EMT: {
         code: "emanateoneos",
